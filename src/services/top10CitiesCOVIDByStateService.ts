9f3bee456888b742f5db6999d36ec03c47bf1eff
Isaac Allef <isaac_allef@hotmail.com>
2021-08-11 17:24:19 -0300

    feat: Choose the top 10 cities with the highest percentage of cases in relation to the total population of the city in the period

--- src/services/top10CitiesCOVIDByStateService.ts
+++ src/services/top10CitiesCOVIDByStateService.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from 'axios';
 
 type params = {
@@ -7,28 +8,62 @@ type params = {
 };
 
 class top10CitiesCOVIDByStateService {
-    async execute(data: params): Promise<any> {
-        const { state, dateStart, dateEnd } = data;
+    async execute(data: params): Promise<Array<any>> {
+        const { state, dateStart } = data;
 
-        const dataApi = this.getDataApi({ state, dateStart, dateEnd });
+        const dataApi = await this.getDataApiBrasilIo(state, dateStart);
 
-        return dataApi;
+        const top10Cities = this.top10CitiesPercentageCasesByTotalPopulation(
+            dataApi.results,
+        );
+
+        return top10Cities;
     }
 
-    private async getDataApi({
-        state,
-        dateStart,
-        dateEnd,
-    }: params): Promise<any> {
+    private top10CitiesPercentageCasesByTotalPopulation(
+        arrayCities: any[],
+    ): Array<any> {
+        const compareByPercentageOfCases = (
+            firstCity: any,
+            secondCity: any,
+        ) => {
+            if (firstCity.percentageOfCases > secondCity.percentageOfCases) {
+                return -1;
+            }
+            return 1;
+        };
+
+        let top10Cities: any[] = [];
+        arrayCities.forEach((city: any) => {
+            if (city.estimated_population && city.confirmed) {
+                const percentageOfCases =
+                    (city.confirmed * 100) / city.estimated_population;
+
+                top10Cities.push({
+                    name: city.city,
+                    percentageOfCases,
+                });
+
+                top10Cities = top10Cities.sort(compareByPercentageOfCases);
+
+                if (top10Cities[10]) {
+                    top10Cities.pop();
+                }
+            }
+        });
+
+        return top10Cities;
+    }
+
+    private async getDataApiBrasilIo(
+        state: string,
+        dateStart: Date,
+    ): Promise<any> {
         const URL = 'https://api.brasil.io/dataset/covid19/caso/data/';
         const token = process.env.TOKEN;
 
         const response = await axios.get(URL, {
-            params: {
-                state,
-                dateStart,
-                dateEnd,
-            },
+            params: { state, date: dateStart.toISOString().split('T')[0] },
             headers: {
                 Authorization: `Token ${token}`,
             },
